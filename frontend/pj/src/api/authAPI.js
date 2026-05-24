1b25a9a15311d28a1c07582f48f981485ce9cd78
Dibesh Ranjan <dibeshranjan6@gmail.com>
2026-05-24 17:41:14 +0530

    update apiurl

--- frontend/pj/src/api/authAPI.js
+++ frontend/pj/src/api/authAPI.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const authAPI = axios.create({
-  baseURL: "http://localhost:8080/api/auth",
+  baseURL: "https://full-stack-furniture-shop-spring-boot.onrender.com/api/auth",
 });
 
 export default authAPI;
